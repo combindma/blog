235a867b0e9d5f53830d74a1f711667fc6b528b6
Omar Herri <herri.omar@gmail.com>
2021-04-15 20:43:11 +0000

    wip

--- resources/views/posts/form.blade.php
+++ resources/views/posts/form.blade.php
@@ -116,8 +116,8 @@
                             <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                         </svg>
                         <div class="flex text-sm text-gray-600">
-                            <label for="images" class="relative cursor-pointer bg-white rounded-md font-medium text-primary-600 hover:text-primary-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-primary-500">
-                                <input id="images" name="images[]" type="file" multiple accept="image/*">
+                            <label for="image" class="relative cursor-pointer bg-white rounded-md font-medium text-primary-600 hover:text-primary-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-primary-500">
+                                <input id="image" name="image" type="file" accept="image/*">
                             </label>
                         </div>
                         <p class="text-xs text-gray-500">
